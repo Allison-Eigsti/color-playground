e39f16ecee5a08ce07e9b66f3c03d0c03ec33d31
AllisonE-code <allisonmeigsti@gmail.com>
2025-10-10 21:34:33 +0200

    Served static html files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Homepage</h1>');
-});
+// Set up static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
