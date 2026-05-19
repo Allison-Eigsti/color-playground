e7a26ac3330c914c00b12dd1f406788a25bce637
AllisonE-code <allisonmeigsti@gmail.com>
2026-05-19 22:00:18 +0200

    Updated file structure added src folder and database file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import palettesRouter from './routes/palettesRouter.js';
-import logger from './middleware/logger.js';
-import errorHandler from './middleware/error.js';
-import notFound from './middleware/notFound.js';
+import palettesRouter from './src/routes/palettesRouter.js';
+import logger from './src/middleware/logger.js';
+import errorHandler from './src/middleware/error.js';
+import notFound from './src/middleware/notFound.js';
+
 
 
 const app = express();
